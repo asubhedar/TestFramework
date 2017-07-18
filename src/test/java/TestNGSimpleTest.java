dec0191d01203a511b46d6b82520e48ca90c81ad
asubhedar <ashwini.subhedar@gmail.com>
2017-07-18 11:50:26 -0400

    First Check In for Framework project

---
+++ src/test/java/TestNGSimpleTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by wofhxfv on 7/18/2017.
+ */
+import org.testng.annotations.Test;
+        import static org.testng.Assert.assertEquals;
+
+public class TestNGSimpleTest {
+    @Test
+    public void testAdd() {
+        String str = "TestNG is working fine";
+        assertEquals("TestNG is working fine", str);
+    }
+}
